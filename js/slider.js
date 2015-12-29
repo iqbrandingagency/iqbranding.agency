284c1036ccfd55571f0994c0bea7683886abb72e
Edwin Xico <contact@xicoofficial.co>
2015-12-28 18:13:35 -0600

    Add arrows to portfolio section

--- js/slider.js
+++ js/slider.js
@@ -1,17 +1,37 @@
-                var currentIndex = 0,
-                items = $('.p-slide'),
-                itemAmt = items.length;
+var currentIndex = 0,
+items = $('.p-slide'),
+itemAmt = items.length;
 
-              function cycleItems() {
-                var item = $('.p-slide').eq(currentIndex);
-                items.hide();
-                item.css('display','table');
-              }
+function cycleItems() {
+  var item = $('.p-slide').eq(currentIndex);
+  items.hide();
+  item.css('display','table');
+}
 
-              var autoSlide = setInterval(function() {
-                currentIndex += 1;
-                if (currentIndex > itemAmt - 1) {
-                  currentIndex = 0;
-                }
-                cycleItems();
-              }, 5000);
+var autoSlide = setInterval(function() {
+  currentIndex += 1;
+  if (currentIndex > itemAmt - 1) {
+    currentIndex = 0;
+  }
+  cycleItems();
+}, 5000);
+
+
+
+$('.wslide-button-next').click(function() {
+  clearInterval(autoSlide);
+  currentIndex += 1;
+  if (currentIndex > itemAmt - 1) {
+    currentIndex = 0;
+  }
+  cycleItems();
+});
+
+$('.wslide-button-prev').click(function() {
+  clearInterval(autoSlide);
+  currentIndex -= 1;
+  if (currentIndex < 0) {
+    currentIndex = itemAmt - 1;
+  }
+  cycleItems();
+});
